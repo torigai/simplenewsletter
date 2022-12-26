305bcd431347f3e8033e98ca4bad19a559b1467d
root <root@regulus.homenet>
2022-12-26 15:23:34 +0100

    footer for newsletter-mails added

--- customvars.php
+++ customvars.php
@@ -25,8 +25,8 @@
 	$setfromname = 'mycompany'; 
 	$addaddress = 'mail@mycompany.de';
 
-	//Logo appearing in every newsletter (could also be an image or nothing)
-	//contoller/send_newsletter.php
+	// Logo and footer appearing in every newsletter
+	// /admin/contoller/send_newsletter.php
 
 	$mylogo = "<div style='margin: 1.5em 0'><h1 style='font-size: 2.7em; font-weight: 400; color: #EF7D25; font-family: \"Roboto Slab\", serif;'>My<span style='color: #333'>Company - Newsletter</span></h1></div>";
 	$myfooter = "";
